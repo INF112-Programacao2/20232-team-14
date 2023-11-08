0e5398d84cc83b54e2a732c431ee0901556fb62d
Isabela <129128103+isabela-cfreitas@users.noreply.github.com>
2023-11-08 14:29:32 -0300

    alteração no controller para preencher vetor de dados do veiculo

--- src/controller/Controller.cpp
+++ src/controller/Controller.cpp
@@ -10,7 +10,7 @@ namespace controller {
 
 
     void Controller::startProgram() {
-
+        std::vector<std::string> _dados_veiculo //responsavel por receber e encaminhar dados dos novos veiculos registrados
         bool loop = true;
         bool erro = false; //responsavel por solicitar novamente entradas consideradas invalidas
         model::Carro c;
@@ -25,7 +25,63 @@ namespace controller {
                     break;
                 case 1:
                     //teria aqui uma instanciação de c
+                    interface.displayOutput("Informe a ordem de servico: ");
+                    _dados_veiculo[0] = interface.readUserInput();
+                    interface.displayOutput("Informe a solicitacao: ");
+                    _dados_veiculo[1] = interface.readUserInput();
+                    interface.displayOutput("Informe o funcionario responsavel por registrar este veiculo: ");
+                    _dados_veiculo[2] = interface.readUserInput();
+                    interface.displayOutput("Informe a placa do veiculo que realizou o reboque: ");
+                    _dados_veiculo[3] = interface.readUserInput();
+                    interface.displayOutput("Informe o motivo da apreensao: ");
+                    _dados_veiculo[4] = interface.readUserInput();
+                    interface.displayOutput("Informe o estado do veiculo no momento de apreensao, considerando:\n");
+                    interface.displayOutput("1 - Ruim\n2 - Regular\n3 - Bom\n");
+                    _dados_veiculo[5] = interface.readUserInput();
+                    interface.displayOutput("Informe se houve blitz, considerando:\n");
+                    interface.displayOutput("**Responda com 1 em caso afirmativo e com 0 em caso negativo**\n");
+                    _dados_veiculo[6] = interface.readUserInput();
+                    interface.displayOutput("Informe o local de apreensao: ");
+                    _dados_veiculo[7] = interface.readUserInput();
+                    interface.displayOutput("Informe a placa do veiculo apreendido: ");
+                    _dados_veiculo[8] = interface.readUserInput();
+                    interface.displayOutput("Informe a marca do veiculo apreendido: ");
+                    _dados_veiculo[9] = interface.readUserInput();
+                    interface.displayOutput("Informe o modelo do veiculo apreendido: ");
+                    _dados_veiculo[10] = interface.readUserInput();
+                    interface.displayOutput("Informe o ano de apreensao do veiculo: ");
+                    _dados_veiculo[11] = interface.readUserInput();
+                    interface.displayOutput("Informe a cidade em que ocorreu a apreensao: ");
+                    _dados_veiculo[12] = interface.readUserInput();
+                    interface.displayOutput("Informe o chassi do veiculo apreendido: ");
+                    _dados_veiculo[13] = interface.readUserInput();
+                    interface.displayOutput("Informe a que distancia do patio o veiculo foi apreendido: ");
+                    _dados_veiculo[14] = interface.readUserInput();
+                    interface.displayOutput("Informe a quilometragem do veiculo no momento de apreensao: ");
+                    _dados_veiculo[15] = interface.readUserInput();
+                    interface.displayOutput("Informe a ocorrencia registrada: ");
+                    _dados_veiculo[16] = interface.readUserInput();
+                    interface.displayOutput("Informe o policial responsavel pela apreensao: ");
+                    _dados_veiculo[17] = interface.readUserInput();
+                    interface.displayOutput("Informe a data de apreensao do veiculo: ");
+                    _dados_veiculo[18] = interface.readUserInput();
+                    interface.displayOutput("Informe o horario de apreensao: ");
+                    _dados_veiculo[19] = interface.readUserInput();
+                    interface.displayOutput("Liste os objetos que estavam no carro no momento de apreensao: ");
+                    _dados_veiculo[20] = interface.readUserInput();
+                    interface.displayOutput("Se desejar, acrescente alguma observacao, caso contrario digite 0 para continuar: ");
+                    _dados_veiculo[21] = interface.readUserInput();
+                    interface.displayOutput("Informe o estado das rodas do veiculo no momento de apreensao, considerando:\n");
+                    interface.displayOutput("0-inexistente\n1-amassado\n2-riscado\n3-quebrado\n4-bom estado\n");
+                    _dados_veiculo[22] = interface.readUserInput();
+                    interface.displayOutput("Informe o estado dos retrovisoes do veiculo no momento de apreensao, considerando:\n");
+                    interface.displayOutput("0-inexistente\n1-amassado\n2-riscado\n3-quebrado\n4-bom estado\n");
+                    _dados_veiculo[23] = interface.readUserInput();
+
+                    model::Veiculo v(_dados_veiculo); //chamando funcao que vai armazenar os dados do veiculo
+                    _dados_veiculo.clear(); //esvaziando o vetor apos seus dados serem transferidos para o objeto veiculo
                     break;
+
                 case 2:
                     interface.displayOutput("*Digite 0 a qualquer momento caso queira desistir da operacao*\n");
                     do {
